c08ec243acf3c726c3a8e0bc11843182762cf1ce
ZpeedX <saboo@kth.se>
2016-10-09 18:59:11 +0200

    tog bort troll kod

--- Rainbow-Software/index.php
+++ Rainbow-Software/index.php
@@ -5,8 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above ll 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content="hej hej jag är en liten apa!">
-    <meta name="author" content="">
+    <meta name="description" content="Rainbow Software">
+    <meta name="author" content="Rainbow Software">
 
     <title>Rainbow Software</title>
 
